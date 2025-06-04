b667206bb13b3c523de32f9d0c52dfbf429a1ab8
caya-karlchristian <karlcaya0704@gmail.com>
2025-06-04 13:15:39 +0800

    Add caya module

---
+++ teamfive/caya.py
@@ -0,0 +1,59 @@
+from os import system
+
+def clear():
+    system("cls")
+    
+def buffer():
+    input("\nPress enter to continue...")
+
+def caya_menu():
+    while True:
+        clear()
+        print("----------Welcome to Caya's Menu----------")
+        print("1. Basic Information")
+        print("2. Goals")
+        print("3. Comment from Arguelles")
+        print("4. Comment from Condino")
+        print("5. Comment from Cordova")
+        print("6. Comment from Gutierrez")
+        print("0. Exit")
+        
+        choice = input("\nPlease enter your choice: ")
+        
+        match choice:
+            case "1":
+                clear()
+                print("----------Basic Information----------")
+                print("Name: Karl Christian M. Caya")
+                print("Age: 20 years old")
+                print("Date of Birth: December 7, 2004")
+                print("Gender: Male")
+                buffer()
+            case "2":
+                clear()
+                print("----------------------Goals----------------------")
+                print("- I want to graduate with flying colors.")
+                print("- Find a decent job that will support my family.")
+                print("- I want to travel the world.")
+                buffer()
+            case "3":
+                clear()
+                # Comment holder for Arguelles
+                buffer()  
+            case "4":
+                clear()
+                # Comment holder for Condino
+                buffer() 
+            case "5":
+                clear()
+                # Comment holder for Cordova
+                buffer()
+            case "6":
+                clear()
+                # Comment holder for Gutierrez
+                buffer()    
+            case "0":
+                break
+            case _:
+                print("\nInvalid choice. Try again!")
+                buffer()
